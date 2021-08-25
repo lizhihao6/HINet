61755fee92fbcb4398b29f02193b7986be068ab5
lizhihao <lizhihao@megvii.com>
2021-08-25 15:59:17 +0800

    add test_v2e.sh

--- scripts/data_preparation/test_v2e.sh
+++ scripts/data_preparation/test_v2e.sh
@@ -1,4 +1,4 @@
-python3 ./.v2e/v2e.py -i input/tennis.mov -o ./.v2e/output \
+python3 ./.v2e/v2e.py -i ./.v2e/input/tennis.mov -o ./.v2e/output \
   --avi_frame_rate=120 --overwrite --auto_timestamp_resolution \
   --output_height 720 --output_width 1280  --dvs_params clean --pos_thres=.15 --neg_thres=.15 \
   --dvs_emulator_seed=0 --slomo_model=./.v2e/.pretrain/SuperSloMo39.ckpt --no_preview \
