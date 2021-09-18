085ede83e9355824ef8f2fabb6db6df0a7980cc2
lizhihao <lizhihao@megvii.com>
2021-09-18 15:54:04 +0800

    fix bug

--- scripts/data_preparation/dvs_genertor.py
+++ scripts/data_preparation/dvs_genertor.py
@@ -190,7 +190,7 @@ def stereo_generate_pairs():
         for s, o in zip(sharp_list, out_paths):
             # copyfile(s, o)
             os.symlink(s, o)
-        pairs.append(dict(sharp_paths=inputs, target_path=out_path) for inputs, out_path in zip(input_list, out_paths))
+        pairs += [dict(sharp_paths=inputs, target_path=out_path) for inputs, out_path in zip(input_list, out_paths)]
 
         input_list = [[p.replace("left", "right") for p in inputs] for inputs in input_list]
         sharp_list = [p.replace("left", "right") for p in sharp_list]
@@ -198,7 +198,7 @@ def stereo_generate_pairs():
         for s, o in zip(sharp_list, out_paths):
             # copyfile(s, o)
             os.symlink(s, o)
-        pairs.append(dict(sharp_paths=inputs, target_path=out_path) for inputs, out_path in zip(input_list, out_paths))
+        pairs += [dict(sharp_paths=inputs, target_path=out_path) for inputs, out_path in zip(input_list, out_paths)]
 
     with open(pairs_save_path, "wb+") as f:
         pickle.dump(pairs, f)
