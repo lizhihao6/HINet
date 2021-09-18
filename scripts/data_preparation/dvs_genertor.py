8044825546d800ad9bba1f9ea603e3686b158fc0
lizhihao <lizhihao@megvii.com>
2021-09-18 17:39:01 +0800

    fix bug

--- scripts/data_preparation/dvs_genertor.py
+++ scripts/data_preparation/dvs_genertor.py
@@ -1,8 +1,9 @@
 import json
 import multiprocessing as mp
-from multiprocessing.dummy import Pool
 import os
 import pickle
+import sys
+from multiprocessing.dummy import Pool
 from pathlib import Path
 
 import cv2
@@ -251,6 +252,11 @@ def gopro_generate_pairs():
 
 if __name__ == '__main__':
     stereo_pairs = stereo_generate_pairs()
-    dvs_genertor = DVS_Genertor(stereo_pairs)
+    idx = int(sys.argv[1])
+    start_id = idx * len(stereo_pairs) // 8
+    stop_id = (idx + 1) * len(stereo_pairs) // 8
+    if len(stereo_pairs) - stop_id <= 8:
+        stop_id = len(stereo_pairs)
+    dvs_genertor = DVS_Genertor(stereo_pairs[start_id:stop_id])
     dvs_genertor.run(["sharps_to_blur", "sharps_to_avi", "avi_to_events", "events_to_voxel"])
     # gopro_pairs = gopro_generate_pairs()
