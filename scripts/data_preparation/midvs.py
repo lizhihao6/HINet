ab9ec3a76b1fddcbf45e23800e8e8c1e0fb0c1dc
lizhihao <lizhihao@megvii.com>
2021-09-18 18:51:57 +0800

    fix bug

--- scripts/data_preparation/midvs.py
+++ scripts/data_preparation/midvs.py
@@ -4,12 +4,30 @@
 # Modified from BasicSR (https://github.com/xinntao/BasicSR)
 # Copyright 2018-2020 BasicSR Authors
 # ------------------------------------------------------------------------
+import os
+from pathlib import Path
+
+import numpy as np
 
 from basicsr.utils.create_lmdb import create_lmdb_for_midvs
 
 
 def main():
-    create_lmdb_for_midvs()
+    # create_lmdb_for_midvs()
+    for p in [str(p) for p in Path("./datasets/MiDVS/*").glob("")]:
+        if len([i for i in os.listdir(p) if i.endswith("txt")]) == 0:
+            continue
+        events = np.zeros([720, 960])
+        for t in [str(s) for s in Path(p).glob("txt")]:
+            with open(t, "r+") as f:
+                for l in f.readlines():
+                    y, x, p = l.split("\n")[0].split(" ")[1:]
+                    y, x, p = int(float(y)), int(float(x)), int(float(p))
+                    if p == 0:
+                        events[y, x] -= 0.2
+                    else:
+                        events[y, x] += 0.2
+        np.save("./datasets/MiDVS/events/{}.npy".format(p.split("/")[-2]), events)
 
 
 if __name__ == '__main__':
