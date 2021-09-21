00021ffe99fac5a11284cf35e504f1972487015e
李智昊(实习) <lizhihao@megvii.com>
2021-09-21 23:10:37 +0800

    fix bug

--- scripts/data_preparation/dvs_genertor.py
+++ scripts/data_preparation/dvs_genertor.py
@@ -39,9 +39,9 @@ COMMAND = "python3 {}/v2e.py " \
           "--avi_frame_rate={} --overwrite --auto_timestamp_resolution --timestamp_resolution=.001 " \
           "--output_height 720 --output_width 1280  --dvs_params %(dvs_params)s --pos_thres={} --neg_thres={} " \
           "--dvs_emulator_seed=0 --slomo_model={} --no_preview --skip_video_output {} " \
-          "--dvs_text=%(output)s ".format(V2E_PATH, FPS, POS_THRES, NEG_THRES, SLOMO_CHECKPOINT,
+          "--dvs_text=%(output)s > /dev/null 2>&1".format(V2E_PATH, FPS, POS_THRES, NEG_THRES, SLOMO_CHECKPOINT,
                                                           APPEND_ARGS)
-# env setting > /dev/null 2>&1
+# env setting 
 GPU_NUM = 8
 CPU_NUM = int(mp.cpu_count())
 # CPU_NUM = 1
@@ -196,7 +196,7 @@ class DVS_Genertor():
                                                                                     'steps': STEPS,
                                                                                     'dvs_params': "clean"}
         print("aaaa", flush=True)
-        # os.system(cmd)
+        os.system(cmd)
         print("bbbb", flush=True)
         cmd = "CUDA_VISIBLE_DEVICES={} ".format(os.getpid() % GPU_NUM) + COMMAND % {'input': avi_path,
                                                                                     'output_folder': tmp_dir,
