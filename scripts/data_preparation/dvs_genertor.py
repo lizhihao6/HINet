ca4a167202c1a08aa280eb9fea701cf3c72f0674
李智昊(实习) <lizhihao@megvii.com>
2021-09-21 17:55:29 +0800

    fix bug

--- scripts/data_preparation/dvs_genertor.py
+++ scripts/data_preparation/dvs_genertor.py
@@ -297,12 +297,13 @@ def gopro_generate_pairs():
 
 if __name__ == '__main__':
     stereo_pairs = stereo_generate_pairs()
-    idx = int(sys.argv[1])
-    start_id = idx * len(stereo_pairs) // 8
-    stop_id = (idx + 1) * len(stereo_pairs) // 8
-    if len(stereo_pairs) - stop_id <= 8:
-        stop_id = len(stereo_pairs)
-    dvs_genertor = DVS_Genertor(stereo_pairs[start_id:stop_id])
+    # idx = int(sys.argv[1])
+    # start_id = idx * len(stereo_pairs) // 8
+    # stop_id = (idx + 1) * len(stereo_pairs) // 8
+    # if len(stereo_pairs) - stop_id <= 8:
+        # stop_id = len(stereo_pairs)
+    # dvs_genertor = DVS_Genertor(stereo_pairs[start_id:stop_id])
+    dvs_genertor = DVS_Genertor(stereo_pairs)
     # dvs_genertor.run(["sharps_to_blur", "sharps_to_avi", "avi_to_events", "events_to_voxel"])
     dvs_genertor.run(["avi_to_voxel"])
     # gopro_pairs = gopro_generate_pairs()
