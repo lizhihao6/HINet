1e336a280913d50f8c2496e942aed58103506235
李智昊(实习) <lizhihao@megvii.com>
2021-09-21 18:20:58 +0800

    fix bug

--- scripts/data_preparation/dvs_genertor.py
+++ scripts/data_preparation/dvs_genertor.py
@@ -188,6 +188,8 @@ class DVS_Genertor():
                                                                                     'diff': diff,
                                                                                     'steps': STEPS,
                                                                                     'dvs_params': "clean"}
+        print(cmd)
+        sys.exit(1)
         os.system(cmd)
         cmd = "CUDA_VISIBLE_DEVICES={} ".format(os.getpid() % GPU_NUM) + COMMAND % {'input': avi_path,
                                                                                     'output': noisy_voxel_path,
