7f7beaf45c2222ef5f2f9153aec719abffd21a8d
李智昊(实习) <lizhihao@megvii.com>
2021-10-15 16:07:09 +0800

    add remap

--- basicsr/models/dvs_image_restoration_model.py
+++ basicsr/models/dvs_image_restoration_model.py
@@ -331,6 +331,7 @@ class DVSImageRestorationModel(BaseModel):
 
         for idx, val_data in enumerate(dataloader):
             img_name = osp.splitext(osp.basename(val_data['lq_path'][0]))[0]
+            print(img_name, val_data['lq_path'])
             # if img_name[-1] != '9':
             #     continue
 
